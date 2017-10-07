a0da333b6406f5b738bf075802689c8088a818e3
Rashik Hasnat <rhasnat93@gmail.com>
2017-10-07 17:56:28 +0600

    Added BusinessLayer

---
+++ Source/PcPool.Inventory.BusinessLayer/UserDataProvider.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using PcPool.DataAccessLayer.PcPoolDBaseModel;
+
+namespace PcPool.Inventory.BusinessLayer
+{
+    public class UserDataProvider
+    {
+        public User VerifyUser(string userName, string password)
+        {
+            var ctx=new PcPoolEntities();
+            var users = ctx.Users.ToList();
+            return null;
+        }
+    }
+}
