6254afa8406bdb8ff9db7fd5f90f9c525a7ba5dc
Rashik Hasnat <rhasnat93@gmail.com>
2017-10-10 02:15:06 +0600

    Added The skeleton Page of item addition

---
+++ Source/Fasetto.Word.Core/ViewModel/AddItemViewModel.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows.Input;
+using Fasetto.Word.Core.ViewModelConverter;
+using PcPool.Inventory.BusinessLayer;
+using PcPool.Inventory.BusinessLayer.Interfaces;
+
+namespace Fasetto.Word.Core
+{
+    /// <summary>
+    /// The View Model for a login screen
+    /// </summary>
+    public class AddItemViewModel : BaseViewModel
+    {
+        //private IInventoryStatProvide _inventoryStatProvide;
+
+        //public ObservableCollection<InventoryItemViewModel> Items { get; set; }
+        ////public TYPE Type { get; set; }
+
+        //public string Test { get; set; } = "Blah Blah";
+
+        //public DashboardViewModel()
+        //{
+        //    _inventoryStatProvide = new InventoryStatProvide();
+        //    var itemList = _inventoryStatProvide.GetInventoryStatus().Select(InventoryItemConverter.InventoryItemStatConverter).ToList();
+        //    Items = new ObservableCollection<InventoryItemViewModel>(itemList);
+        //}
+    }
+}
---
+++ Source/Fasetto.Word/Pages/AddItemPage.xaml.cs
@@ -0,0 +1,18 @@
+﻿using Fasetto.Word.Core;
+using System.Security;
+
+namespace Fasetto.Word
+{
+    /// <summary>
+    /// Interaction logic for RegisterPage.xaml
+    /// </summary>
+    public partial class AddItemPage : BasePage<AddItemViewModel>
+    {
+        public AddItemPage()
+        {
+            InitializeComponent();
+        }
+
+       
+    }
+}
